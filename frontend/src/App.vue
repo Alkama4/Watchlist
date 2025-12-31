ff035a6da2932290a9f4cf36532ebcce54223e0d
Aleksi Malkki <aleksi.malkki@gmail.com>
2025-12-31 11:32:30 +0200

    Change to top nav layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,73 +1,106 @@
 <script setup></script>
 
 <template>
-    <div class="side-by-side">
-        <aside>
-            <header>
+    <header>
+        <nav>
+            <router-link to="/" class="no-deco">
                 <h2 class="name">Watchlist</h2>
-            </header>
-            <nav>
+            </router-link>
+            <form role="search">
+                <input type="search" placeholder="Search for titles">
+            </form>
+            <div class="flex-row align-center">
                 <ul>
-                    <li><router-link to="/">Home</router-link></li>
-                    <li><router-link to="/account">Account</router-link></li>
-                    <li><router-link to="/login">Login</router-link></li>
+                    <li>
+                        <router-link 
+                            class="btn btn-text no-deco" 
+                            to="/"
+                        >
+                            <i class="bx bxs-compass"></i>
+                            Discover
+                        </router-link>
+                    </li>
+                    <li>
+                        <router-link 
+                            class="btn btn-text no-deco" 
+                            to="/collections"
+                        >
+                            <i class="bx bxs-collection"></i>
+                            Collections
+                        </router-link>
+                    </li>
                 </ul>
-                <img src="" alt="Profile icon">
-            </nav>
-        </aside>
-
-        <main>
-            <header>
-                <form role="search">
-                    <input type="search" placeholder="Search for titles">
-                </form>
-            </header>
-
-            <div class="content">
-                <router-view/>
+                <router-link 
+                    class="btn no-deco btn-user"
+                    to="/account"
+                >
+                    <i class="bx bxs-user"></i>
+                </router-link>
             </div>
+        </nav>
+    </header>
 
-            <footer>
-                © Aleksi Malkki 2026
-            </footer>
-        </main>
-    </div>
+    <main>
+        <router-view/>
+    </main>
+
+    <footer>
+        © Aleksi Malkki 2026
+    </footer>
 </template>
 
 <style scoped>
-.side-by-side {
-    display: flex;
-    flex-direction: row;
-    width: 100vw;
-}
-
-aside {
-    box-sizing: border-box;
-    height: 100vh;
-    width: 250px;
-    padding: var(--spacing-sm) var(--spacing-md);
-
-    background-color: var(--c-bg-section);
-}
-
-main {
-    overflow-y: scroll;
-    min-height: 100vh;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-}
-
 header {
-    padding: var(--spacing-md);
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    padding: var(--spacing-sm-md) var(--spacing-md);
+    border-bottom: 1px solid var(--c-border);
+    backdrop-filter: blur(30px);
+    background-color: rgba(0, 0, 0, 0.66);
+}
+
+nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.name {
+    margin: 0;
+    color: var(--c-text);
 }
 
 input[type="search"] {
     margin: 0;
 }
 
-.content {
+
+ul {
+    list-style-type: none;
+    display: flex;
+    padding: 0;
+    margin: 0;
+    /* gap: var(--spacing-sm); */
+}
+
+header .btn {
+    padding: var(--spacing-sm) var(--spacing-sm-md);
+    font-size: var(--fs-0);
+    display: flex;
+    align-items: center;
+}
+
+.btn-user {
+    border-radius: 100px;
+    padding: 10px !important;
+    font-size: var(--fs-1) !important;
+}
+
+main {    
     padding-inline: var(--spacing-md);
+    margin-top: 63px;
 }
 
 footer {
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,8 @@ import { useAuthStore } from '@/stores/auth'
 import '@/assets/variables.css'
 import '@/assets/fonts.css'
 import '@/assets/base.css'
+import '@/assets/utilities.css'
+import 'boxicons/css/boxicons.min.css'
 
 const app = createApp(App)
 const pinia = createPinia()
