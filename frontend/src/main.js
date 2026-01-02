dfb0db26e092a61f0f448354859c107079817389
Aleksi Malkki <aleksi.malkki@gmail.com>
2026-01-02 16:15:07 +0200

    Add `classes.css` and improve existing rules

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,7 @@ import '@/assets/variables.css'
 import '@/assets/fonts.css'
 import '@/assets/base.css'
 import '@/assets/utilities.css'
+import '@/assets/classes.css'
 import 'boxicons/css/boxicons.min.css'
 
 const app = createApp(App)
