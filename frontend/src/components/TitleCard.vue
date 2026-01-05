8cbd3d71c37de1cc5ad999e349d754d80a4f06da
Aleksi Malkki <aleksi.malkki@gmail.com>
2026-01-05 17:05:02 +0200

    Finalize title card layout - Add svg loader - Fill in rest of the data in the title card

--- frontend/src/components/TitleCard.vue
+++ frontend/src/components/TitleCard.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { resolveImagePath } from '@/utils/imagePath';
 import { timeFormatters } from '@/utils/formatters';
+import Tmdb from '@/assets/icons/tmdb.svg'
 
 defineProps({
     titleInfo: {
@@ -21,15 +22,26 @@ defineProps({
             :alt="`${titleInfo?.type === 'tv' ? 'TV show' : 'Movie'} poster: ${titleInfo?.name}`"
         >
         <div class="details">
-            <h3>{{ titleInfo?.name }}</h3>
+            <h5>{{ titleInfo?.name }}</h5>
             <div class="detail-row">
+                <Tmdb/>
                 {{ titleInfo?.tmdb_vote_average }}
                 &bull;
                 {{ timeFormatters.timestampToYear(titleInfo?.release_date) }}
             </div>
             <div class="detail-row">
-                <span>{{ timeFormatters.minutesToHrAndMin(titleInfo?.movie_runtime) }}</span>
+                <template v-if="titleInfo?.type === 'tv'">
+                    {{ titleInfo?.show_season_count }} seasons,
+                    {{ titleInfo?.show_episode_count }} episodes
+                </template>
+                <template v-else>{{ timeFormatters.minutesToHrAndMin(titleInfo?.movie_runtime) }}</template>
             </div>
+        </div>
+        <div v-if="titleInfo?.user_details?.watch_count" class="watch-count-marker">
+            <template v-if="titleInfo?.user_details?.watch_count >= 2">
+                {{ titleInfo?.user_details?.watch_count }}
+            </template>
+            <i v-else class="bx bx-check"></i>
         </div>
     </router-link>
 </template>
@@ -41,6 +53,7 @@ a {
     display: flex;
     flex-direction: column;
     text-decoration: none;
+    position: relative;
 }
 
 img {
@@ -56,8 +69,11 @@ img {
     margin-top: var(--spacing-xs-sm);
 }
 
-h3 {
+h5 {
     margin: 0;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
 }
 
 .detail-row {
@@ -66,5 +82,23 @@ h3 {
     gap: var(--spacing-xs);
     color: var(--c-text-3);
     font-size: var(--fs-neg-1);
+}
+
+.watch-count-marker {
+    position: absolute;
+    top: var(--spacing-sm);
+    left: var(--spacing-sm);
+    background-color: var(--c-positive);
+    width: var(--spacing-lg);
+    height: var(--spacing-lg);
+    border-radius: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 500;
+    color: var(--c-text);
+}
+.watch-count-marker i {
+    font-size: var(--fs-3);
 }
 </style>
