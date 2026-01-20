d3fff2762ffe254a3e6b383e66d02a120c1675e7
Aleksi Malkki <aleksi.malkki@gmail.com>
2026-01-20 03:09:58 +0200

    Work on title details page layout

--- frontend/src/views/TitleDetailsPage.vue
+++ frontend/src/views/TitleDetailsPage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import LoadingButton from '@/components/LoadingButton.vue';
 import { fastApi } from '@/utils/fastApi';
+import { timeFormatters } from '@/utils/formatters';
 import { resolveImagePath } from '@/utils/imagePath';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
@@ -86,87 +87,104 @@ onMounted(async () => {
             :alt="`${titleDetails?.type} backdrop: ${titleDetails?.name}`"
             class="backdrop"
         >
-
+        
         <img 
-            :src="resolveImagePath(titleDetails, 'original', 'poster')"
-            :alt="`${titleDetails?.type} poster: ${titleDetails?.name}`"
-            class="poster"
+            :src="resolveImagePath(titleDetails, 'original', 'logo')"
+            :alt="`${titleDetails?.type} logo: ${titleDetails?.name}`"
+            class="logo"
         >
 
-        <h1>{{ titleDetails?.name }}</h1>
-        <q>{{ titleDetails?.tagline }}</q>
-
-        <div class="actions">
-            <i
-                class="bx bxs-heart btn btn-text btn-square"
-                :class="{'btn-favourite': titleDetails?.user_details?.is_favourite }"
-                @click="toggleFavourite"
-            ></i>
-            <i
-                class="bx bxs-time btn btn-text btn-square"
-                :class="{'btn-accent': titleDetails?.user_details?.in_watchlist }"
-                @click="toggleWatchlist"
-            ></i>
-            <i
-                class="bx bxs-collection btn btn-text btn-square"
-                @click="adjustCollections"
-            ></i>
-
-            <!-- Move actions below to a drop down -->
-
-            <i
-                v-if="titleDetails?.user_details?.in_library"
-                class="bx bx-list-minus btn btn-text btn-square"
-                @click="removeFromLibrary"
-            ></i>
-            <i
-                v-else
-                class="bx bx-list-plus btn btn-text btn-square"
-                @click="addToLibrary"
-            ></i>
-
-            <LoadingButton
-                @click="updateTitleDetails"
-                :loading="waitingFor?.titleUpdate"
+        <div class="main-info">
+            <img 
+                :src="resolveImagePath(titleDetails, 'original', 'poster')"
+                :alt="`${titleDetails?.type} poster: ${titleDetails?.name}`"
+                class="poster"
             >
-                Update title details
-            </LoadingButton>
-            <!-- <a href="">Choose different images</a> -->
+            
+            <div class="right-side">
+                <h1 class="name">
+                    {{ titleDetails?.name }}
+                    ({{ timeFormatters.timestampToYear(titleDetails?.release_date) }})
+                </h1>
+                <h4 class="name-original" v-if="titleDetails?.name_original != titleDetails?.name">
+                    {{ titleDetails?.name_original }}
+                </h4>
+                <q class="tagline">{{ titleDetails?.tagline }}</q>
+        
+                <div class="actions">
+                    <i
+                        class="bx bxs-heart btn btn-text btn-square"
+                        :class="{'btn-favourite': titleDetails?.user_details?.is_favourite }"
+                        @click="toggleFavourite"
+                    ></i>
+                    <i
+                        class="bx bxs-time btn btn-text btn-square"
+                        :class="{'btn-accent': titleDetails?.user_details?.in_watchlist }"
+                        @click="toggleWatchlist"
+                    ></i>
+                    <i
+                        class="bx bxs-collection btn btn-text btn-square"
+                        @click="adjustCollections"
+                    ></i>
+        
+                    <!-- Move actions below to a drop down -->
+        
+                    <i
+                        v-if="titleDetails?.user_details?.in_library"
+                        class="bx bx-list-minus btn btn-text btn-square"
+                        @click="removeFromLibrary"
+                    ></i>
+                    <i
+                        v-else
+                        class="bx bx-list-plus btn btn-text btn-square"
+                        @click="addToLibrary"
+                    ></i>
+        
+                    <LoadingButton
+                        @click="updateTitleDetails"
+                        :loading="waitingFor?.titleUpdate"
+                    >
+                        Update title details
+                    </LoadingButton>
+                    <!-- <a href="">Choose different images</a> -->
+                </div>
+                <div class="flex-row align-center gap-sm" style="margin-top: var(--spacing-md);">
+                    <button @click="addToTitleWatchCount">Add</button>
+                    <div>Watched {{ titleDetails?.user_details?.watch_count }} times</div>
+                    <button @click="removeFromTitleWatchCount">Remove</button>
+                </div>
+                <p>{{ titleDetails?.overview }}</p>
+                <div class="links">
+                    <a
+                        :href="`https://www.themoviedb.org/${titleDetails?.type}/${titleDetails?.tmdb_id}`"
+                        target="_blank"
+                        class="btn no-deco"
+                    >
+                        TMDB
+                        <i class="bx bx-link-external"></i>
+                    </a>
+                    <a
+                        v-if="titleDetails?.imdb_id"
+                        :href="`https://www.imdb.com/title/${titleDetails?.imdb_id}`"
+                        target="_blank"
+                        class="btn no-deco"
+                    >
+                        IMDB
+                        <i class="bx bx-link-external"></i>
+                    </a>
+                    <a
+                        v-if="titleDetails?.homepage"
+                        :href="titleDetails?.homepage"
+                        target="_blank"
+                        class="btn no-deco"
+                    >
+                        Homepage
+                        <i class="bx bx-link-external"></i>
+                    </a>
+                </div>
+            </div>
         </div>
-        <div class="flex-row align-center gap-sm" style="margin-top: var(--spacing-md);">
-            <button @click="addToTitleWatchCount">Add</button>
-            <div>Watched {{ titleDetails?.user_details?.watch_count }} times</div>
-            <button @click="removeFromTitleWatchCount">Remove</button>
-        </div>
-        <p>{{ titleDetails?.overview }}</p>
-        <div class="links">
-            <a
-                :href="`https://www.themoviedb.org/${titleDetails?.type}/${titleDetails?.tmdb_id}`"
-                target="_blank"
-                class="btn no-deco"
-            >
-                TMDB
-                <i class="bx bx-link-external"></i>
-            </a>
-            <a
-                v-if="titleDetails?.imdb_id"
-                :href="`https://www.imdb.com/title/${titleDetails?.imdb_id}`"
-                target="_blank"
-                class="btn no-deco"
-            >
-                IMDB
-                <i class="bx bx-link-external"></i>
-            </a>
-            <a
-                v-if="titleDetails?.homepage"
-                :href="titleDetails?.homepage"
-                target="_blank"
-                class="btn no-deco"
-            >
-                Homepage
-                <i class="bx bx-link-external"></i>
-            </a>
-        </div>
+
         <p style="color: var(--c-text-3)">{{ titleDetails }}</p>
     </div>
 </template>
@@ -184,12 +202,51 @@ img.backdrop {
     mask-image: linear-gradient(
         to top,
         rgba(0, 0, 0, 0) 0%,
-        rgba(0, 0, 0, 0.25) 35%
+        rgba(0, 0, 0, 0.25) 50%
     );
 }
+
+img.logo {
+    object-fit: contain;
+    object-position: left center;
+
+    width: 100%;
+    height: 10vh;
+    margin: 17.5vh 0;
+    box-sizing: border-box;
+}
+
+.main-info {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    gap: var(--spacing-md-lg);
+}
+
+/* .right-side {
+    background-color: rgba(0, 0, 0, 0.226);
+    backdrop-filter: blur(8px);
+    padding: var(--spacing-md-lg);
+    border-radius: var(--border-radius-lg);
+    border: 1px solid var(--c-border);
+} */
+
+
 img.poster {
-    width: 200px;
-    border-radius: var(--border-radius-md);
+    width: 300px;
+    border-radius: var(--border-radius-lg);
+}
+
+.name {
+    margin: 0;
+}
+.name-original {
+    margin: 0;
+}
+.tagline {
+    display: block;
+    margin: var(--spacing-md) 0;
+    color: var(--c-text-1);
+    font-style: italic;
 }
 
 .actions {
