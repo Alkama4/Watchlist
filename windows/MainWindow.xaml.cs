44e524b0cc38d2627521a61bcd5ca71c4a564278
Aleksi Malkki <aleksi.malkki@gmail.com>
2026-01-28 18:06:54 +0200

    Setup WebView2 for windows client

--- windows/MainWindow.xaml.cs
+++ windows/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,8 +6,16 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WinRT.Interop;
+using Microsoft.UI.Windowing;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +30,29 @@ namespace Watchlist
         public MainWindow()
         {
             InitializeComponent();
+            SetTitleBarColors();
+        }
+
+        private void SetTitleBarColors()
+        {
+            IntPtr hwnd = WindowNative.GetWindowHandle(this);
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
+
+            AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+
+            AppWindowTitleBar titleBar = appWindow.TitleBar;
+
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+                titleBar.BackgroundColor = Colors.Black;
+                titleBar.ForegroundColor = Colors.White;
+
+                titleBar.ButtonBackgroundColor = Colors.Black;
+                titleBar.ButtonForegroundColor = Colors.White;
+
+                titleBar.ButtonHoverBackgroundColor = Colors.DarkGray;
+                titleBar.ButtonPressedBackgroundColor = Colors.Gray;
+            }
         }
     }
 }
