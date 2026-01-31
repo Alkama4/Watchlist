fef08df8a6ea48a595a8a9776313a667c2eea0ad
Aleksi Malkki <aleksi.malkki@gmail.com>
2026-01-31 20:49:30 +0200

    Change runtime formatting

--- frontend/src/utils/formatters.js
+++ frontend/src/utils/formatters.js
@@ -8,7 +8,7 @@ export const timeFormatters = {
         const mins = minutes % 60;
 
         if (hrs === 0) return `${mins} min`;
-        return `${hrs} hr, ${mins} min`;
+        return `${hrs} hr ${mins} min`;
     },
     timestampToYear: (timestamp) => {
         return timestamp ? new Date(timestamp).getFullYear() : '-';
