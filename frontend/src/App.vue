17a1f2a75aca4c4a12db6faefb2a3ac868afba8c
Aleksi Malkki <aleksi.malkki@gmail.com>
2026-02-08 17:27:57 +0200

    Fix view reload when navigating between season and title - Fixes unnescary backend requests - Might cause issues with remembering scroll, but seems to work between title and season at the very least

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -64,7 +64,7 @@ function onSearchSubmit() {
     </header>
 
     <main :class="{'header-visible': route.meta.requiresAuth && !route.meta.disableHeaderPadding}">
-        <router-view :key="$route.fullPath"/>
+        <router-view/>
     </main>
 
     <footer>
