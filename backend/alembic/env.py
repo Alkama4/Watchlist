222536e3fb77bd667941ae14df806f3cdfda943c
Aleksi Malkki <aleksi.malkki@gmail.com>
2026-05-22 22:01:02 +0300

    Adjust `alembic/env.py` imports and comments

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -8,25 +8,21 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+# The app_config and app.models need to be imported,even if not
+# directly called in this file. The imports load them to memory
+# so that alembic can use them.
 from app import config as app_config
 from app.database import Base
 import app.models
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+# Used to get the values from the .ini file.
 config = context.config
-
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
 target_metadata = Base.metadata
 
-# Get the database url from enviroment variables
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./test.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "")
 
 
 def run_migrations_offline() -> None:
