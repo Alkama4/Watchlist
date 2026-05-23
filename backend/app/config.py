94bd0bbfce9876782924e9a4d205906677a331b1
Aleksi Malkki <aleksi.malkki@gmail.com>
2026-05-23 11:08:55 +0300

    Move the `.env.sample` to project root

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,5 @@
 import os
 from dotenv import load_dotenv
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../.env.local"))
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../../.env"))
 
 DEFAULT_MAX_QUERY_LIMIT = 50
