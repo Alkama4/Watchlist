2b2150db548603ac3543a2824dbcc1a2367a26d2
Aleksi Malkki <aleksi.malkki@gmail.com>
2026-05-24 12:50:29 +0300

    Init docker-compose and Dockerfiles - Also setup/update everything to work properly with them e.g. nginx.conf and .env.sample

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,9 +2,15 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = os.environ["DATABASE_URL"]
+db_host = os.environ.get("DB_HOST", 'localhost')
+db_port = os.environ.get("DB_PORT", '5432')
+db_name = os.environ.get("DB_NAME", 'watchlist')
+db_user = os.environ.get("DB_USER", 'postgres')
+db_pass = os.environ.get("DB_PASSWORD", 'postgres')
 
-engine = create_async_engine(DATABASE_URL, echo=False)
+database_url = f"postgresql+asyncpg://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+
+engine = create_async_engine(database_url, echo=False)
 
 AsyncSessionLocal = async_sessionmaker(
     engine,
