1674f600ec47b418ea5f0f43c51ed2d17668166f
Aleksi Malkki <aleksi.malkki@gmail.com>
2026-05-24 13:46:30 +0300

    Fix Alembic setup issues - Add missing entry to `requirements.txt`. - Build database url in `env.py` using the seperate variables.

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -22,7 +22,13 @@ if config.config_file_name is not None:
 
 target_metadata = Base.metadata
 
-DATABASE_URL = os.getenv("DATABASE_URL", "")
+DB_USER = os.getenv("DB_USER", "")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_HOST = os.getenv("DB_HOST", "")
+DB_PORT = os.getenv("DB_PORT", "")
+DB_NAME = os.getenv("DB_NAME", "")
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 def run_migrations_offline() -> None:
